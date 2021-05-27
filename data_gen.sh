c8da7c522902153b67dcd324e20748a42138b00f
Parvathi Madathil Kooloth <paru@Parvathis-MacBook-Pro.local>
2021-05-27 15:17:20 -0500

    added execute statements for level 1 run

--- data_gen.sh
+++ data_gen.sh
@@ -1,1 +1,2 @@
-.practice-repo/PeleC/PeleC/Exec/RegTests/PMF/PeleC3d.gnu.ex practice-repo/PeleC/PeleC/Exec/RegTests/PMF/inputs_ex
+mpirun -np 2 ../practice-repo/PeleC/PeleC/Exec/RegTests/PMF/PeleC3d.gnu.ex ./../practice-repo/PeleC/PeleC/Exec/RegTests/PMF/inputs_ex>base_run.txt 
+mpirun -np 2 ../practice-repo/PeleC/PeleC/Exec/RegTests/PMF/PeleC3d.gnu.ex ./../practice-repo/PeleC/PeleC/Exec/RegTests/PMF/inputs_ex1>lvl1_run.txt 
